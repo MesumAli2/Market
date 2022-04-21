d91e9ab1cfea34c7bb2dc6305a82b637ba897f20
MesumAli2 <smesumali20@gmail.com>
2022-04-21 15:10:15 +0400

    BottomNavigation with click listners

--- app/src/main/java/com/mesum/market/MainActivity.kt
+++ app/src/main/java/com/mesum/market/MainActivity.kt
@@ -2,10 +2,48 @@ package com.mesum.market
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mesum.market.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setUpBottomBar()
+
     }
+
+    private fun setUpBottomBar() {
+        val bottomNavigationView : BottomNavigationView = binding.bottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener(object: BottomNavigationView.OnNavigationItemSelectedListener{
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when(item.itemId){
+                    R.id.menu_home ->{
+                        Toast.makeText(this@MainActivity, "Home", Toast.LENGTH_SHORT).show()
+                    }
+                    R.id.menu_fav ->{
+                        Toast.makeText(this@MainActivity, "Favourite", Toast.LENGTH_SHORT).show()
+                    }
+                    R.id.menu_place_add ->{
+                        Toast.makeText(this@MainActivity, "Add", Toast.LENGTH_SHORT).show()
+                    }
+                    R.id.menu_profile -> {
+                        Toast.makeText(this@MainActivity, "User", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                return true
+            }
+
+        })
+    }
+
+
 }
