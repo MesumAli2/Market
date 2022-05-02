34a184d690b66dc88b8e1855eb6252e46f151732
MesumAli2 <smesumali20@gmail.com>
2022-05-02 13:16:42 +0400

    Added RecyclerViewSupport

--- app/src/main/java/com/mesum/market/MainActivity.kt
+++ app/src/main/java/com/mesum/market/MainActivity.kt
@@ -2,14 +2,16 @@ package com.mesum.market
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import android.widget.TextView
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mesum.market.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private  var marketData  = mutableListOf<String>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +20,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setUpBottomBar()
+        for (i in 0 until 10){
+            marketData.add("Product ${i.toString()}")
+        }
+
+        setupRecyclerView()
+
 
     }
+
+    class RvViewHolder(val view: View) : RecyclerView.ViewHolder(view){
+    }
+
+    fun setupRecyclerView(){
+        val recyclerViewAdapter = object  : RecyclerView.Adapter<RvViewHolder>(){
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvViewHolder {
+                return  RvViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_category, parent, false))
+            }
+
+            override fun onBindViewHolder(holder: RvViewHolder, position: Int) {
+                holder.itemView.findViewById<TextView>(R.id.product_text).text = marketData[position]
+            }
+
+            override fun getItemCount(): Int {
+                return  marketData.size
+            }
+
+        }
+        val recyclerView : RecyclerView = binding.recyclerViewRent
+        recyclerView.adapter = recyclerViewAdapter
+    }
+
+
+
 
     private fun setUpBottomBar() {
         val bottomNavigationView : BottomNavigationView = binding.bottomNavigationView
